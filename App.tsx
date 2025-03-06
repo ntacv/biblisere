465f45947b6627b6b2fe7aaff926b882b5b041a8
Nathan Choukroun <nchoukroun@cardiweb.com>
2025-03-06 15:12:18 +0100

    WIP: config i18n not working

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { useTranslation } from "react-i18next";
+import i18next from "./localization/i18n";
 
 export default function App() {
+  const { t } = useTranslation();
   return (
     <View style={styles.container}>
-      <Text>Plongeoir Nathan</Text>
+      <Text>Plongeoir Nathan; Langue: {t("language")}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ localization/i18n.ts
@@ -0,0 +1,18 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./en.json";
+import fr from "./fr.json";
+const resources = {
+  en,
+  fr,
+};
+
+i18next.use(initReactI18next).init({
+  resources,
+  lng: "en", // default language to use.
+  // if you're using a language detector, do not define the lng option
+  interpolation: {
+    escapeValue: false,
+  },
+});
+export default i18next;
