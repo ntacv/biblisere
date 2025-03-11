6cb4f930328a9525cd12f5241bc80a30e07c3f8d
Nathan Choukroun <nchoukroun@cardiweb.com>
2025-03-11 15:39:34 +0100

    feat: absolute paths with babel

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: ["babel-preset-expo"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./"],
+        alias: {
+          "@assets": "./assets",
+          "@components": "./components",
+          "@localization": "./localization",
+          "@styles": "./styles",
+          "@screens": "./screens",
+          "@utils": "./utils",
+        },
+      },
+    ],
+  ],
+};
