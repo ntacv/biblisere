bcd3e1ad676dbbed9153e0d4914f7487582b3ad9
Nathan Choukroun <nchoukroun@cardiweb.com>
2025-03-13 16:27:47 +0100

    fix: useTranslation and i18n instead of TextTranslated

--- localization/ChooseLanguage.tsx
+++ localization/ChooseLanguage.tsx
@@ -2,17 +2,18 @@ import React, { useTransition } from "react";
 import { TouchableOpacity, View, Text } from "react-native";
 
 import { useTranslation } from "react-i18next";
+import i18next from "localization/i18n";
 
 const ChooseLanguage = () => {
-  const { i18n, t } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <View>
       <Text>{t("config:language")}</Text>
-      <TouchableOpacity onPress={() => i18n.changeLanguage("en")}>
+      <TouchableOpacity onPress={() => i18next.changeLanguage("en")}>
         <Text>{t("components:translate:en")}</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => i18n.changeLanguage("fr")}>
+      <TouchableOpacity onPress={() => i18next.changeLanguage("fr")}>
         <Text>{t("components:translate:fr")}</Text>
       </TouchableOpacity>
     </View>
