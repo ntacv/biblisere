e2adae5726c05c6ff23a1e4a8c95a201012712b6
Nathan Choukroun <nchoukroun@cardiweb.com>
2025-03-13 15:32:27 +0100

    fix: MergeRequest corrections - en.ts to en.json

--- localization/ChooseLanguage.tsx
+++ localization/ChooseLanguage.tsx
@@ -4,17 +4,16 @@ import { TouchableOpacity, View, Text } from "react-native";
 import TextTranslated from "localization/TextTranslated";
 import i18next from "localization/i18n";
 
-const ChooseLanguage = () => {
-  return (
-    <View>
-      <TextTranslated>config:language</TextTranslated>
-      <TouchableOpacity onPress={() => i18next.changeLanguage("en")}>
-        <TextTranslated>components:translate:en</TextTranslated>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => i18next.changeLanguage("fr")}>
-        <TextTranslated>components:translate:fr</TextTranslated>
-      </TouchableOpacity>
-    </View>
-  );
-};
+const ChooseLanguage = () => (
+  <View>
+    <TextTranslated>config:language</TextTranslated>
+    <TouchableOpacity onPress={() => i18next.changeLanguage("en")}>
+      <TextTranslated>components:translate:en</TextTranslated>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => i18next.changeLanguage("fr")}>
+      <TextTranslated>components:translate:fr</TextTranslated>
+    </TouchableOpacity>
+  </View>
+);
+
 export default ChooseLanguage;
