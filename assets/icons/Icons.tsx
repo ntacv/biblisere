e664bbece240463b90fec134899b99c5fc2738f7
Nathan Choukroun <nchoukroun@cardiweb.com>
2025-03-25 15:11:15 +0100

    feat: new icons arrow

--- assets/icons/Icons.tsx
+++ assets/icons/Icons.tsx
@@ -3,6 +3,8 @@ import mapPin from "assets/icons/map-pin.svg";
 import user from "assets/icons/user.svg";
 import search from "assets/icons/search.svg";
 import book from "assets/icons/book.svg";
+import arrowRight from "assets/icons/arrow-right.svg";
+import arrowDown from "assets/icons/arrow-down.svg";
 
 const IconSvgs = {
   menu: menu,
@@ -10,6 +12,8 @@ const IconSvgs = {
   user: user,
   search: search,
   book: book,
+  arrowRight: arrowRight,
+  arrowDown: arrowDown,
 };
 export type iconType = keyof typeof IconSvgs;
 
