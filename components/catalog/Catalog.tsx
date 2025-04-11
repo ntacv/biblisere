10617b59d1597ed3867702a39b0494a59d9867ee
Nathan Choukroun <nchoukroun@cardiweb.com>
2025-04-11 16:43:13 +0200

    feat: front filter categories TODO call api then apply search

--- components/catalog/Catalog.tsx
+++ components/catalog/Catalog.tsx
@@ -1,16 +1,22 @@
 import * as React from 'react';
 import { ScrollView, Text, View } from 'react-native';
 
+import { IconNames } from 'assets/icons/Icons';
 import { useStoreMap } from 'effector-react';
 import { useTranslation } from 'react-i18next';
 import * as StoreBooks from 'stores/books';
 import styled from 'styled-components/native';
-import { fonts, sizes } from 'styles/Variables';
+import { colors, fonts, sizes } from 'styles/Variables';
+
+import { Api, Category } from 'api/apiSwagger';
 
 import ViewPage from 'components/ViewPage';
 import BookListItem from 'components/book/BookListItem';
+import Button from 'components/button/Button';
 import ContainerColumn from 'components/utils/ContainerColumn';
 import Searchbar from 'components/utils/Searchbar';
+
+const api = new Api();
 
 interface Props {
 	route: {
@@ -26,10 +32,12 @@ const Catalog = (props) => {
 	const propSearch = props.route.params?.search;
 	const [search, setSearch] = React.useState('');
 	const [searchedBooks, setSearchedBooks] = React.useState<number[]>([]);
+	const [categories, setCategories] = React.useState({ open: false, categories: [] });
+	const [filters, setFilters] = React.useState([]);
 
 	const storeBooks = useStoreMap(StoreBooks.store, (store) => store);
 
-	const filterBooks = () => {
+	const searchBooks = () => {
 		// Filter books based on search input
 		setSearchedBooks(
 			storeBooks.books
@@ -43,7 +51,7 @@ const Catalog = (props) => {
 	};
 	React.useEffect(() => {
 		if (search !== '') {
-			filterBooks();
+			searchBooks();
 		}
 	}, [search]);
 
@@ -53,14 +61,73 @@ const Catalog = (props) => {
 		}
 	}, [propSearch]);
 
+	React.useEffect(() => {
+		if (filters.length > 0) {
+			searchBooks();
+		}
+	}, [filters]);
+
+	React.useEffect(() => {
+		const categoryUnique = storeBooks.books
+			.flatMap((book) => book.categories)
+			.reduce(
+				(acc, val) => {
+					acc[val.id] = val;
+					return acc;
+				},
+				{} as Record<number, Category>,
+			);
+
+		setCategories((filters) => ({
+			...filters,
+			categories: Object.values(categoryUnique),
+		}));
+	}, []);
+
 	return (
 		<ViewPage header>
 			<ScrollViewContent>
 				<ContainerColumn>
-					<Searchbar value={{ search, setSearch }} onPress={filterBooks} />
-					<Text style={{ alignSelf: 'flex-end' }}>
-						{(search !== '' ? searchedBooks.length : storeBooks.books.length) + t('catalog:result')}
-					</Text>
+					<Searchbar value={{ search, setSearch }} onPress={searchBooks} />
+					<View>
+						<Button
+							label={t('catalog:filter')}
+							iconName={IconNames.arrowDown}
+							onPress={() => setCategories((filters) => ({ ...filters, open: !filters.open }))}
+						/>
+						{categories.open && (
+							<ViewInline>
+								{categories.categories.map((category, index) =>
+									filters.find((filter) => filter.id === category.id) ? (
+										<TextSelected
+											key={index}
+											onPress={() => {
+												setFilters((filters) =>
+													filters.filter((filter) => filter.id !== category.id),
+												);
+											}}
+										>
+											{category.name + ', '}
+										</TextSelected>
+									) : (
+										<TextToSelect
+											key={index}
+											onPress={() => {
+												setFilters((filters) => [...filters, category]);
+											}}
+										>
+											{category.name + ', '}
+										</TextToSelect>
+									),
+								)}
+							</ViewInline>
+						)}
+
+						<Text style={{ alignSelf: 'flex-end' }}>
+							{(search !== '' ? searchedBooks.length : storeBooks.books.length) +
+								t('catalog:result')}
+						</Text>
+					</View>
 					<ViewList>
 						{storeBooks.books ? (
 							search !== '' ? (
@@ -87,6 +154,20 @@ const ScrollViewContent = styled(ScrollView)`
 const ViewList = styled(View)`
 	gap: ${sizes.padding.main}px;
 `;
+const ViewInline = styled(View)`
+	padding: ${sizes.padding.main}px;
+	flex-direction: row;
+	flex-wrap: wrap;
+`;
 const TextContent = styled(Text)`
 	font: ${fonts.content};
 `;
+const TextCategory = styled(Text)`
+	font: ${fonts.content};
+	color: ${colors.content};
+	padding: 3px;
+`;
+const TextSelected = styled(TextCategory)`
+	background-color: ${colors.primary};
+`;
+const TextToSelect = styled(TextCategory)``;
